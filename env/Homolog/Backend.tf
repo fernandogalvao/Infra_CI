c24196d0aa3ed1a9b6003fc94f93034e6ca222a1
FernandoGalvao <90917242+fernandogalvao@users.noreply.github.com>
2024-07-17 20:59:12 -0300

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
-    key    = "Prod/terraform.tfstate"
-    region = "us-west-2"
+    bucket = "terraform-state-ci-gip"
+    key    = "Homolog/terraform.tfstate"
+    region = "us-east-1"
   }
 }

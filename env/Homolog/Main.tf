364c1d369bbcc51aa30e7086898c232ce2c58a2e
FernandoGalvao <90917242+fernandogalvao@users.noreply.github.com>
2024-07-18 14:21:18 -0300

    Update Main.tf

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -4,7 +4,7 @@ module "homolog" {
     nome_repositorio = "homolog"
     cargoIAM = "homolog"
     ambiante = "homolog"
-    imagem="leonardosartorello/go_ci:22"
+    imagem="fernandoagalvao/go_ci:20"
 }
 
 output "IP_alb" {
